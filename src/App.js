bdbbcaf23cc8ed96532e61a67668732e6df0f38e
predrag jandric <predrag.jandric.bg@gmail.com>
2023-08-23 21:42:07 +0200

    empty project template initiated

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure, minima.</p>
+    </div>
+  );
+}
+
+export default App;
